ad7eb427c5afc238b5c06e244a74001e2c0437dd
James Kevin C. Jamili <jameskevin.ramesessystems@gmail.com>
2023-11-08 09:44:12 +0800

    updated

--- app/Component/MenuButton.tsx
+++ app/Component/MenuButton.tsx
@@ -17,14 +17,12 @@ const MenuButton: React.FC<MenuButtonProps> = ({
   return (
     <div className="mt-10 capitalize">
       <Link href={link} className="text-[#335F96]">
-        <div className="bg-white rounded-3xl w-full h-52 shadow-[-25px_25px_15px_-10px_rgba(0,0,0,0.4)]">
-          <div className="flex">
-            <div className="ml-14 pt-6">{iconImg}</div>
-            <div className="flex justify-center items-center">
-              <div className="grid-cols-2 m-12 font-serif">
-                <div className="text-[40px] font-bold">{title}</div>
-                <div className="text-xl text-[#1b1b1b]">{description}</div>
-              </div>
+        <div className="bg-white rounded-3xl w-full h-52 shadow-[-25px_25px_15px_-10px_rgba(0,0,0,0.4)] flex items-center border-2">
+          <div className="ml-14 pt-6">{iconImg}</div>
+          <div className="flex justify-center items-center">
+            <div className="grid-cols-2 m-12 font-serif">
+              <div className="text-[40px] font-bold">{title}</div>
+              <div className="text-xl text-[#1b1b1b]">{description}</div>
             </div>
           </div>
         </div>
