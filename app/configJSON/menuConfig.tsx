1ac6226abe401e30315a922ce67d766cb7fcc1d8
James Kevin C. Jamili <jameskevin.ramesessystems@gmail.com>
2023-11-10 14:21:14 +0800

    New upated

--- app/configJSON/menuConfig.tsx
+++ app/configJSON/menuConfig.tsx
@@ -7,25 +7,21 @@ const menuConfig = [
     link: "/queueing",
     iconImg: <BsTicketDetailed size={130} className=" text-[#1b1b1b]" />,
     title: "Queue Number",
-    description: "Lorem dolor sit amet",
   },
   {
     link: "/pay-business",
     iconImg: <GiPayMoney size={130} className="text-[#1b1b1b]" />,
     title: "Pay Business",
-    description: "Lorem dolor sit amet consectetur",
   },
   {
     link: "/pay-property",
     iconImg: <BsBuildings size={130} className="text-[#1b1b1b]" />,
     title: "Pay Property",
-    description: "Lorem dolor sit amet consectetur.",
   },
   {
     link: "/other-payments",
     iconImg: <MdOutlineOtherHouses size={130} className="text-[#1b1b1b]" />,
     title: "Other Payments",
-    description: "Lorem dolor sit amet consectetur.",
   },
 ];
 
