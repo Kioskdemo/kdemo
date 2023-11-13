8d1e2afd9172ff83170cccce53793ae9d122b837
James Kevin C. Jamili <jameskevin.ramesessystems@gmail.com>
2023-11-13 09:02:11 +0800

    Newly update

--- app/Component/MenuButton.tsx
+++ app/Component/MenuButton.tsx
@@ -11,10 +11,12 @@ const MenuButton: React.FC<MenuButtonProps> = ({ link, iconImg, title }) => {
   return (
     <div className="capitalize">
       <Link href={link} className="text-[#335F96]">
-        <div className="w-[270px] h-[270px] bg-white rounded-[25%] text-[#335F96] shadow-[-20px_22px_15px_-10px_rgba(0,0,0,0.3)] border border-[#335F96] flex flex-col items-center justify-center gap-5">
+        <div className="w-[200px] h-[200px] bg-white rounded-[50%] text-[#335F96] shadow-[-20px_22px_15px_-10px_rgba(0,0,0,0.3)] border border-[#335F96] flex flex-col items-center justify-center gap-5">
+          {" "}
           <div className="">{iconImg}</div>
-          <h1 className="text-[25px] font-bold">{title}</h1>
         </div>
+
+        <h1 className="text-[25px] font-bold">{title}</h1>
       </Link>
     </div>
   );
